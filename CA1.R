37294864aaa2b08f594e68c27bc28242d669c7cb
luan_navarro <luan_navarro@hotmail.com>
2023-11-19 15:53:59 +0000

    Commit 7 - Final
    Minor improvements in readability and graphs visualization.

--- CA1.R
+++ CA1.R
@@ -1,58 +1,58 @@
 #a
-# Load necessary libraries
+#Loading the necessary libraries.
 library(factoextra)
 library(corrplot)
 library(tidyverse)
 library(ggplot2)
 library(naniar)
 
-# Load the dataset
+#Loading the dataset.
 data <- read.csv("data.csv", header = TRUE)
 
-# Convert categorical variables to human-readable labels
+#Converting categorical variables to human-readable labels.
 data$SEX <- factor(data$SEX, levels = c(1, 2), labels = c("Female", "Male"))
 data$ICU <- factor(data$ICU, levels = c(1, 2), labels = c("Admitted", "Not Admitted"))
 
-# Convert boolean features (1 and 2) to categorical 'Yes' and 'No'
+#Converting boolean features (1 and 2) to categorical 'Yes' and 'No'.
 boolean_features <- c("PNEUMONIA", "DIABETES", "HIPERTENSION", "OTHER_DISEASE", "CARDIOVASCULAR", "OBESITY", "RENAL_CHRONIC")
 data[boolean_features] <- lapply(data[boolean_features], function(x) {
   factor(x, levels = c(1, 2), labels = c("Yes", "No"))
 })
 
-# Replace coded missing values (97, 98, 99) with standard NA
+#Replacing coded missing values (97, 98, 99) with standard NA.
 data[data == 97 | data == 98 | data == 99] <- NA
 
-# Check data structure to identify variable types (categorical, discrete, continuous)
+#Checking data structure to identify variable types (categorical, discrete, continuous).
 str(data)
 
-# Visualize distributions to further understand data type and check for missing values
+#Visualizing distributions to further understand data type and check for missing values.
 for(var in names(data)) {
   print(
     if(is.factor(data[[var]])) {
       ggplot(data, aes(x = .data[[var]])) + 
-        geom_bar() +
+        geom_bar(fill = "green", color = "black") +
         theme_minimal() +
         labs(title = paste("Distribution of", var))
     } else {
       ggplot(data %>% filter(!is.na(.data[[var]])), aes(x = .data[[var]])) + 
-        geom_histogram(binwidth = 1) +
+        geom_histogram(binwidth = 1, fill = "green", color = "black") +
         theme_minimal() +
         labs(title = paste("Distribution of", var))
     }
   )
 }
 
-# Visualize patterns of missing data, if applicable
+#Visualizing patterns of missing data, if applicable.
 missing_cols <- sum(colSums(is.na(data)) > 0)
 if (missing_cols >= 2) {
   gg_miss_upset(data)
 }
 
-# Display count of missing values per variable
+#Displaying count of missing values per variable.
 colSums(is.na(data))
 
 #b
-# Calculate descriptive statistics for AGE (mean, median, min, max, and SD)
+#Calculating descriptive statistics for AGE (mean, median, min, max, and SD).
 age_stats <- data.frame(
   Mean = mean(data$AGE, na.rm = TRUE),
   Median = median(data$AGE, na.rm = TRUE),
@@ -63,59 +63,59 @@ age_stats <- data.frame(
 print(age_stats)
 
 #c
-# Scale AGE using different techniques
+#Scaling AGE using different techniques
 
-# Min-Max normalization
+#Min-Max normalization.
 min_val <- min(data$AGE, na.rm = TRUE)
 max_val <- max(data$AGE, na.rm = TRUE)
 data$AGE_min_max <- (data$AGE - min_val) / (max_val - min_val)
 
-# Z-score standardization
+#Z-score standardization.
 mean_val <- mean(data$AGE, na.rm = TRUE)
 sd_val <- sd(data$AGE, na.rm = TRUE)
 data$AGE_z_score <- (data$AGE - mean_val) / sd_val
 
-# Robust scaling using IQR
+#Robust scaling using IQR.
 q1 <- quantile(data$AGE, 0.25, na.rm = TRUE)
 q3 <- quantile(data$AGE, 0.75, na.rm = TRUE)
 data$AGE_robust <- (data$AGE - q1) / (q3 - q1)
 
-# Display first few rows of original and transformed AGE
+#Display first few rows of original and transformed AGE.
 print(data[, c("AGE", "AGE_min_max", "AGE_z_score", "AGE_robust")][1:10, ])
 
 #d
-# Calculate and visualize correlations between numeric features
+#Calculating and visualizing correlations between numeric features.
 
-# Calculate correlation matrix
+#Calculating correlation matrix.
 cor_matrix <- cor(data[sapply(data, is.numeric)], use = "complete.obs")
 
-# Create heatmap for visual representation of correlations
+#Creating heatmap for visual representation of correlations.
 corrplot(cor_matrix, method = "color", type = "upper", title = "Heatmap of Correlations", tl.cex = 0.7)
 
 #e
-# Further exploration and visualization
+#Further exploration and visualization.
 
-# Correlation between AGE and ICU
+#Correlation between AGE and ICU.
 cor_age_icu <- cor.test(data$AGE, as.numeric(data$ICU), method = "pearson", use = "complete.obs")
 print(paste("Correlation between Age and ICU Admission: ", round(cor_age_icu$estimate, 3)))
 
-# Bar plots for comorbidity frequencies in ICU patients
+#Bar plots for comorbidity frequencies in ICU patients.
 comorbidity_vars <- c("PNEUMONIA", "DIABETES", "HIPERTENSION", "OTHER_DISEASE", "CARDIOVASCULAR", "OBESITY", "RENAL_CHRONIC")
 for(var in comorbidity_vars) {
   print(
     ggplot(data[data$ICU == "Admitted",], aes(x = .data[[var]])) +
-      geom_bar() +
+      geom_bar(fill = "green", color = "black") +
       theme_minimal() +
       labs(title = paste("Frequency of", var, "among ICU Admitted Patients"))
   )
 }
 
-# Grouping and visualizing AGE in categories
+#Grouping and visualizing AGE in categories.
 data$age_group <- cut(data$AGE, breaks = c(0, 18, 35, 50, 65, 100), labels = c("0-18", "19-35", "36-50", "51-65", "66+"), include.lowest = TRUE)
 
-# Display age group distributions and ICU admissions by age group
+#Displaying age group distributions and ICU admissions by age group.
 print(ggplot(data, aes(x = age_group)) +
-        geom_bar() +
+        geom_bar(fill = "green", color = "black") +
         theme_minimal() +
         labs(title = "Distribution of Patients by Age Groups"))
 
@@ -125,33 +125,33 @@ print(ggplot(data, aes(x = age_group, fill = ICU)) +
         labs(title = "ICU Admissions by Age Groups"))
 
 #f
-# One-hot encode the 'SEX' variable
+#One-hot encode the 'SEX' variable.
 
-# Generate dummy variables
+#Generating dummy variables.
 data_dummies <- model.matrix(~SEX - 1, data)
 data_dummies <- as.data.frame(data_dummies)
 data <- cbind(data, data_dummies)
 
-# Check encoding results
+#Checking encoding results.
 print(head(data[, c("SEXFemale", "SEXMale")]))
 
 #g
-# Perform PCA on cleaned dataset
+#Performing PCA on cleaned dataset.
 
-# Filter out rows with missing values
+#Filtering out rows with missing values.
 clean_data <- data[complete.cases(data), ]
 
-# Standardize numeric data
+#Standardizing numeric data.
 numeric_data_clean <- clean_data[, sapply(clean_data, is.numeric)]
 standardized_data_clean <- scale(numeric_data_clean, center = TRUE, scale = TRUE)
 
-# Apply PCA
+#Applying PCA.
 pca_result_clean <- prcomp(standardized_data_clean)
 
-# Summary and visualization of PCA results
+#Summary and visualization of PCA results.
 summary(pca_result_clean)
 print(fviz_eig(pca_result_clean))
 
-# Display loadings for first two principal components
+#Displaying loadings for first two principal components.
 loadings_clean <- pca_result_clean$rotation[, 1:2]
 print(loadings_clean)
